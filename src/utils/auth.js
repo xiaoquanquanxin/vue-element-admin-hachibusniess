d3e3c8d44707bd12feec60db41c7e90e9f895f2b
quanxin <quanxin@pujitech.com>
2020-05-20 17:54:56 +0800

    token改为sessionstorage

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,18 +1,14 @@
-import Cookies from 'js-cookie';
-
 const TokenKey = 'Admin-Token';
 
-export function getToken(){
-    return Cookies.get(TokenKey);
+export function getToken() {
+    return sessionStorage.getItem(TokenKey);
 }
 
-export function setToken(token){
+export function setToken(token) {
     console.log('设置token：', TokenKey, token);
-    return Cookies.set(TokenKey, token);
+    return sessionStorage.setItem(TokenKey, token);
 }
 
-export function removeToken(){
-    // console.log(removeToken);
-    // return;
-    return Cookies.remove(TokenKey);
+export function removeToken() {
+    sessionStorage.removeItem(TokenKey);
 }
