ced322a26a0084b2edb48bddee90839bd1acb2d5
quanxin <quanxin@pujitech.com>
2020-05-20 18:53:45 +0800

    總結

--- src/permission.js
+++ src/permission.js
@@ -55,6 +55,7 @@ router.beforeEach(async (to, from, next) => {
             }
         }
     } else {
+        debugger;
         if (whiteList.indexOf(to.path) !== -1) {
             // in the free login whitelist, go directly
             next();
